9c32a371403a05c3771817dab00253d7c7451ddd
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-08-27 18:16:44 +0900

    ログイン機能の実装#10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'homes#top'
+  resources :homes
   resources :events
   resources :tournaments
 end
