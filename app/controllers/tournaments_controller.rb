a0026192200649df481b1acec6f954cda6cb52b2
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-08-29 10:41:59 +0900

    コントローラの設定#15

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -2,4 +2,12 @@ class TournamentsController < ApplicationController
   def index
     @tournaments = Tournament.all
   end
+
+  def new
+    @tournament = Tournament.new
+  end
+
+  def create
+    @tournament = Tournament.new
+  end
 end
