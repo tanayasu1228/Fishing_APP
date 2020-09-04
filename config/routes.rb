95e33a0e851ce99bb07322b6dba07ba3d8efa71b
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-09-04 14:36:03 +0900

    ルーティングにmy_showを追加#16

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'homes#top'
-  resources :users
+  resources :users do
+    collection do
+      get :my_show
+    end
+  end
   resources :homes
   resources :events
   resources :tournaments do
