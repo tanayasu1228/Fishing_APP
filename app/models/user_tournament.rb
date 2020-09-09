2056ddc6f8c229d1741412fa28b1c48ecc964aba
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-09-09 21:34:22 +0900

    モデルを作成#26

---
+++ app/models/user_tournament.rb
@@ -0,0 +1,2 @@
+class UserTournament < ApplicationRecord
+end
---
+++ test/models/user_tournament_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserTournamentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
