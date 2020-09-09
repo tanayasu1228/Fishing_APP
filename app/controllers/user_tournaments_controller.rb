a5ccb1c599faf564a9f105fdb5d985d1359f9fed
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-09-09 21:44:10 +0900

    中間テーブルを作成＆コントローラの作成#26

---
+++ app/controllers/user_tournaments_controller.rb
@@ -0,0 +1,2 @@
+class UserTournamentsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_121237) do
+ActiveRecord::Schema.define(version: 2020_09_09_123254) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -58,6 +58,13 @@ ActiveRecord::Schema.define(version: 2020_09_06_121237) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "user_tournaments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tournament_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
