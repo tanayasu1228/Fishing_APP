dd5cabbc64ac8a75b45f8a507d595785b9a0c75e
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-09-11 14:01:16 +0900

    コントローラを作成#27

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,19 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def cretae
+    
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
 end
