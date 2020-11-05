be3e8a047d358883b0dc33828d3a840325bfbc59
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2020-11-05 12:08:12 +0900

    エントリー後のリダイレクト先を変更#60

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,13 +2,13 @@ class EntriesController < ApplicationController
   def create
     @entry = current_user.entries.build(tournament_id: params[:tournament_id])
     @entry.save
-    redirect_to tournaments_path
+    redirect_to tournament_path(params[:tournament_id])
   end
 
   def destroy
     @entry = Entry.find_by(tournament_id: params[:tournament_id], user_id: current_user.id)
     @entry.destroy
-    redirect_to tournaments_path
+    redirect_to tournament_path(params[:tournament_id])
   end
 end
 
