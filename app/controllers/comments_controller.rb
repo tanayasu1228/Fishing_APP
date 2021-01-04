ce9cfb90423990ceb3c15aa158454f0b0a3241b0
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2021-01-04 09:13:58 +0900

    コメント返信機能のAjax化を実装#77

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :set_comment_instances
+  
   def create
-    @tournament = Tournament.find(params[:tournament_id])
-    @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
-    @comment_reply = @post.comments.build
     @comment.save!
   end
 
   def destroy
-    
-    Comment.find(params[:id]).destroy
-    
-    # @comment = Comment.find(params[:id])
-    # @comment.destroy
-
-    # @comment_reply = Comment.find(params[:id])
-    # @comment_reply.destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
   end
 
   private
@@ -23,6 +16,11 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:body, :parent_id).merge(post_id: params[:post_id], tournament_id: params[:tournament_id])
   end
+
+  def set_comment_instances
+    @tournament = Tournament.find(params[:tournament_id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments
+    @comment_reply = @post.comments.build
+  end
 end
-
-
