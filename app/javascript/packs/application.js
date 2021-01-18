f7f7334e7f1121df9615ea40490c6fc3562fa7ee
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2021-01-18 12:02:13 +0900

    大会開催タブ表示の読み込みバグを改善#107

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("jquery")
 require("bootstrap/dist/js/bootstrap")
 
 require("./top/slide")
