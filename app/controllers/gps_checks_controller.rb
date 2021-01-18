41912cd944a7c4c7021420265efe21b58979088e
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2021-01-18 14:58:14 +0900

    GPSデータのチェックページの実装#109

---
+++ app/controllers/gps_checks_controller.rb
@@ -0,0 +1,22 @@
+class GpsChecksController < ApplicationController
+
+  def new
+    @gps_check = GpsCheck.new
+  end
+
+  def create
+    @gps_check = GpsCheck.new(gps_check_params)
+    img = Magick::ImageList.new(Rails.root.to_s + "/public#{@gps_check.image.url}")
+    get_exif = img.get_exif_by_entry('GPSLatitude')
+    if get_exif[0][1].nil?
+      redirect_to new_gps_check_path, alert: "チェック結果： GPSデータがありません。カメラの位置情報設定を変更してください"
+    else
+      redirect_to new_gps_check_path, notice: "チェック結果： 写真からGPSデータを確認されました。投稿ができる状態です" 
+    end
+  end
+
+    private
+  def gps_check_params
+    params.require(:gps_check).permit(:image)
+  end
+end
