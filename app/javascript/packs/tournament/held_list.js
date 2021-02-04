7f5949d786cb208407c2d6df9ff07360481bb023
tanahashiyasuhiro <tana.yasu1228@gmail.com>
2021-02-04 21:12:43 +0900

    タブデザインを変更#125

--- app/javascript/packs/tournament/held_list.js
+++ app/javascript/packs/tournament/held_list.js
@@ -1,14 +1,9 @@
-$('.tab-content>div').hide();
-$('.tab-content>div').first().slideDown();
-$('.tab-buttons span').click(function () {
-  var thisclass = $(this).attr('class');
-  $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
-  $('.tab-content>div').each(function () {
-    if ($(this).hasClass(thisclass)) {
-      $(this).fadeIn(800);
-    }
-    else {
-      $(this).hide();
-    }
-  });
-});
+$(function () {
+  let tabs = $(".tab");
+  $(".tab").on("click", function () {
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    const index = tabs.index(this);
+    $(".content").removeClass("show").eq(index).addClass("show");
+  })
+})
